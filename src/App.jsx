3aa6d0926cad2c1ab0bc7e71b85330e8451cf083
bayuik <bayuindrakusuma05@gmail.com>
2022-02-28 07:43:41 +0700

    add header, footer, and notes container

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Heading from "./components/Header";
+import Footer from './components/Footer';
+import Note from './components/Note';
+import './styles/style.css'
+
+const App = () => {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+};
+export default App;
